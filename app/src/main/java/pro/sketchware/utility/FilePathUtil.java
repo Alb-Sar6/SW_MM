b8969f0d2d675a1f53fcd9bd6d0ab45839a07558
Mahmoud-Magd <112110992+Mahmoud-Magd@users.noreply.github.com>
2026-06-03 19:44:55 +0300

    Refactor FilePathUtil methods to improve formatting
    Added notes

--- app/src/main/java/pro/sketchware/utility/FilePathUtil.java
+++ app/src/main/java/pro/sketchware/utility/FilePathUtil.java
@@ -4,14 +4,12 @@ import android.os.Environment;
 
 import java.io.File;
 
-public class FilePathUtil {
-    
-    private static final File SKETCHWARE_DATA = new File(Environment.getExternalStorageDirectory(), ".sketchware/data/");
-    private static final File SKETCHWARE_LOCAL_LIBS = new File(Environment.getExternalStorageDirectory(), ".sketchware/libs/local_libs");
-    
-    
-    
-    // NOTE:
+
+// =========================================================
+// FilePathUtil
+// =========================================================
+
+// NOTE:
         // ALL METHODS IN THIS CLASS HAVE BEEN MADE STATIC.
     // WHY:
         // It improves performance
@@ -28,26 +26,62 @@ public class FilePathUtil {
         // Whenever u get free,
             // replace: new FilePathUtil().method()
             // With: FilePathUtil.method()
+        
+    // ONCE ALL INSTANCE CALLS ARE REPLCED WITH STATIC CALLS IN THIS PROJECT,
+        // UNCOMMENT THE PRIVATE CONSTRUCTOR "private FilePathUtil() {}"
+        // TO PREVENT USERS FROM MAKING NEW INSTANCES
+
+// =========================================================
+
+
+public class FilePathUtil {
+    
+    
+    
+    
+    
+    // =========================================================
+    // CONSTANTS
+    // =========================================================
+    private static final File SKETCHWARE_DATA = new File (Environment.getExternalStorageDirectory(), ".sketchware/data/");
+    private static final File SKETCHWARE_LOCAL_LIBS = new File (Environment.getExternalStorageDirectory(), ".sketchware/libs/local_libs");
+    
+    
+    
+    
+    
+    // =========================================================
+    // CONSTRUCTOR
+    // =========================================================
+    // private FilePathUtil() {}
+    
+    
+    
+    
+    
+    // =========================================================
+    // PUBLIC METHODS
+    // =========================================================
     
     // =========================================================
     // File returns
     // =========================================================
     
     // sc_id = projectId
-    public static File getProjectDir(String sc_id) {
-        return new File(SKETCHWARE_DATA, sc_id);
+    public static File getProjectDir (String sc_id) {
+        return new File (SKETCHWARE_DATA, sc_id);
     }
     
-    public static File getProjectFilesDir(String sc_id) {
-        return new File(getProjectDir(sc_id), "files");
+    public static File getProjectFilesDir (String sc_id) {
+        return new File (getProjectDir (sc_id), "files");
     }
     
-    public static File getDirJava(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "java");
+    public static File getDirJava (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "java");
     }
     
-    public static File getLocalLibraryDir(String libraryName) {
-        return new File(SKETCHWARE_LOCAL_LIBS, libraryName);
+    public static File getLocalLibraryDir (String libraryName) {
+        return new File (SKETCHWARE_LOCAL_LIBS, libraryName);
     }
     
     
@@ -57,12 +91,12 @@ public class FilePathUtil {
     // =========================================================
     
     // ======= Folders (3) =======
-    public static String getPathSvg(String sc_id) {
-        return new File(getProjectDir(sc_id), "converted-vectors").getAbsolutePath();
+    public static String getPathSvg (String sc_id) {
+        return new File (getProjectDir (sc_id), "converted-vectors").getAbsolutePath();
     }
     
-    public static String getPathProjectFilesDir(String sc_id) {
-        return getProjectFilesDir(sc_id).getAbsolutePath();
+    public static String getPathProjectFilesDir (String sc_id) {
+        return getProjectFilesDir (sc_id).getAbsolutePath();
     }
     
     // TODO: make method for "Injection"
@@ -70,52 +104,52 @@ public class FilePathUtil {
     
     
     // ======= Files (19) =======
-    public static String getManifestBroadcast(String sc_id) {
-        return new File(getProjectDir(sc_id), "broadcast").getAbsolutePath();
+    public static String getManifestBroadcast (String sc_id) {
+        return new File (getProjectDir (sc_id), "broadcast").getAbsolutePath();
     }
     
     // TODO: make method for "build_config"
     
     // TODO: make method for "command"
     
-    public static String getLastCompileLogPath(String sc_id) {
-        return new File(getProjectDir(sc_id), "compile_log").getAbsolutePath();
+    public static String getLastCompileLogPath (String sc_id) {
+        return new File (getProjectDir (sc_id), "compile_log").getAbsolutePath();
     }
     
     // TODO: make method for "file"
     
-    public static String getPathImport(String sc_id) {
-        return new File(getProjectDir(sc_id), "import").getAbsolutePath();
+    public static String getPathImport (String sc_id) {
+        return new File (getProjectDir (sc_id), "import").getAbsolutePath();
     }
     
-    public static String getManifestJava(String sc_id) {
-        return new File(getProjectDir(sc_id), "java").getAbsolutePath();
+    public static String getManifestJava (String sc_id) {
+        return new File (getProjectDir (sc_id), "java").getAbsolutePath();
     }
     
     // TODO: make method for "library"
     
-    public static String getPathLocalLibrary(String sc_id) {
-        return new File(getProjectDir(sc_id), "local_library").getAbsolutePath();
+    public static String getPathLocalLibrary (String sc_id) {
+        return new File (getProjectDir (sc_id), "local_library").getAbsolutePath();
     }
     
     // TODO: make method for "logic"
     
-    public static String getPathPermission(String sc_id) {
-        return new File(getProjectDir(sc_id), "permission").getAbsolutePath();
+    public static String getPathPermission (String sc_id) {
+        return new File (getProjectDir (sc_id), "permission").getAbsolutePath();
     }
     
     // TODO: make method for "proguard"
     
-    public static String getPathProguard(String sc_id) {
-        return new File(getProjectDir(sc_id), "proguard-rules.pro").getAbsolutePath();
+    public static String getPathProguard (String sc_id) {
+        return new File (getProjectDir (sc_id), "proguard-rules.pro").getAbsolutePath();
     }
     
     // TODO: make method for "project_config"
     
     // TODO: make method for "resource"
     
-    public static String getManifestService(String sc_id) {
-        return new File(getProjectDir(sc_id), "service").getAbsolutePath();
+    public static String getManifestService (String sc_id) {
+        return new File (getProjectDir (sc_id), "service").getAbsolutePath();
     }
     
     // TODO: make method for "stringfog"
@@ -129,8 +163,8 @@ public class FilePathUtil {
     // =========================================================
     // inside (.sketchware/data/{projectId}/converted-vectors)
     // =========================================================
-    public static String getSvgFullPath(String sc_id, String resName) {
-        return new File(getPathSvg(sc_id), resName + ".svg").getAbsolutePath();
+    public static String getSvgFullPath (String sc_id, String resName) {
+        return new File (getPathSvg (sc_id), resName + ".svg").getAbsolutePath();
     }
     
     
@@ -140,44 +174,44 @@ public class FilePathUtil {
     // =========================================================
     
     // ======= Folders (10) =======
-    public static String getPathAssets(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "assets").getAbsolutePath();
+    public static String getPathAssets (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "assets").getAbsolutePath();
     }
     
-    public static String getPathBroadcast(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "broadcast").getAbsolutePath();
+    public static String getPathBroadcast (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "broadcast").getAbsolutePath();
     }
     
-    public static String getPathJava(String sc_id) {
-        return getDirJava(sc_id).getAbsolutePath();
+    public static String getPathJava (String sc_id) {
+        return getDirJava (sc_id).getAbsolutePath();
     }
     
-    public static String getPathKotlinCompilerPlugins(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "kt_plugins").getAbsolutePath();
+    public static String getPathKotlinCompilerPlugins (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "kt_plugins").getAbsolutePath();
     }
     
-    public static String getJarPathLocalLibraryUser(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "library" + File.separator + "jar").getAbsolutePath();
+    public static String getJarPathLocalLibraryUser (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "library" + File.separator + "jar").getAbsolutePath();
     }
     
-    public static String getDexPathLocalLibraryUser(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "library" + File.separator + "dex").getAbsolutePath();
+    public static String getDexPathLocalLibraryUser (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "library" + File.separator + "dex").getAbsolutePath();
     }
     
-    public static String getResPathLocalLibraryUser(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "library" + File.separator + "res").getAbsolutePath();
+    public static String getResPathLocalLibraryUser (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "library" + File.separator + "res").getAbsolutePath();
     }
     
-    public static String getPathNativelibs(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "native_libs").getAbsolutePath();
+    public static String getPathNativelibs (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "native_libs").getAbsolutePath();
     }
     
-    public static String getPathResource(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "resource").getAbsolutePath();
+    public static String getPathResource (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "resource").getAbsolutePath();
     }
     
-    public static String getPathService(String sc_id) {
-        return new File(getProjectFilesDir(sc_id), "service").getAbsolutePath();
+    public static String getPathService (String sc_id) {
+        return new File (getProjectFilesDir (sc_id), "service").getAbsolutePath();
     }
     
     
@@ -185,20 +219,22 @@ public class FilePathUtil {
     // =========================================================
     // SKETCHWARE_LOCAL_LIBS → inside (.sketchware/libs/local_libs)
     // =========================================================
-    public static String getJarPathLocalLibrary(String libraryName) {
-        return new File(getLocalLibraryDir(libraryName), "classes.jar").getAbsolutePath();
+    public static String getJarPathLocalLibrary (String libraryName) {
+        return new File (getLocalLibraryDir (libraryName), "classes.jar").getAbsolutePath();
     }
     
-    public static String getDexPathLocalLibrary(String libraryName) {
-        return new File(getLocalLibraryDir(libraryName), "classes.dex").getAbsolutePath();
+    public static String getDexPathLocalLibrary (String libraryName) {
+        return new File (getLocalLibraryDir (libraryName), "classes.dex").getAbsolutePath();
     }
     
-    public static String getResPathLocalLibrary(String libraryName) {
-        return new File(getLocalLibraryDir(libraryName), "res").getAbsolutePath();
+    public static String getResPathLocalLibrary (String libraryName) {
+        return new File (getLocalLibraryDir (libraryName), "res").getAbsolutePath();
     }
-        
-        
-        
+    
+    
+    
+    
+    
 }
 
 
